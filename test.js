e2be89b1a79c9490f26bdac25247738ad30f22ba
tunnckoCore <mameto_100@mail.bg>
2015-03-26 23:58:22 +0200

    start adding tests

--- test.js
+++ test.js
@@ -11,5 +11,20 @@ var assert = require('assert');
 var npmPkgs = require('./index');
 
 describe('npm-pkgs:', function() {
-  // body
+  it('should work properly when existing user given', function(done) {
+    this.timeout(30000);
+    npmPkgs('tunnckocore', function _cb(err, res) {
+      assert.ifError(err);
+      assert.strictEqual(res.length > 90, true);
+      done();
+    });
+  });
+  it('should error when non existing user given', function(done) {
+    this.timeout(30000);
+    npmPkgs('fjk43hkjhhhhhhhhhhhhhhhkjgg3k4g234', function _cb(err, res) {
+      assert.strictEqual(err instanceof Error, true);
+      assert.strictEqual(res, undefined);
+      done();
+    });
+  });
 });
